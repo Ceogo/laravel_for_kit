9ec9d8b86b85403c9ce553258957fecb5df59a22
Ceogo <sydykov.rus@yandex.kz>
2024-10-19 18:52:27 +0500

    deleted:    app/Http/Controllers/Api/WorkerController.php 	deleted:    app/Http/Controllers/Auth/LoginController.php 	deleted:    app/Http/Controllers/Auth/RegisterController.php 	new file:   app/Http/Controllers/LoginController.php 	new file:   app/Http/Controllers/RegisterController.php 	deleted:    app/Models/Students.php 	deleted:    app/Models/Workers.php 	modified:   bootstrap/app.php 	modified:   composer.json 	modified:   composer.lock 	deleted:    config/sanctum.php 	deleted:    database/factories/StudentsFactory.php 	deleted:    database/factories/WorkersFactory.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
-
-Route::get('register', [RegisterController::class, 'create'])->name('register');
-Route::post('register', [RegisterController::class, 'store']);
-
-Route::get('login', [LoginController::class, 'create'])->name('login');
-Route::post('login', [LoginController::class, 'store']);
-
-Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
+});
